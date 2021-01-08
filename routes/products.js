d86c96784a21c2655615bb2185fe449ae4893db1
VictorFiant <77025466+VictorFiant@users.noreply.github.com>
2021-01-08 12:17:14 +0000

    cambios

--- routes/products.js
+++ routes/products.js
@@ -1,19 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var productsControllers = require("../controllers/productsControllers")
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  const productos = [
-    {
-      id:1,
-      name:"Fiat 1"
-    },
-    {
-      id:2,
-      name:"Fiat 2"
-    }
-  ]
-  res.json(productos);
-});
+router.get('/', productsControllers.getAll);
 
 module.exports = router;
