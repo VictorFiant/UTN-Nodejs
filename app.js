a9b1729a50f78b18dce0ad44babc6b8a97d544f1
VictorFiant <77025466+VictorFiant@users.noreply.github.com>
2021-01-13 12:35:46 +0000

    cambios en app.js

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// variable sin alperar la app //
+var ProductController = require('./controllers/ProductController');
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var routerControllers = require('./routes/products');
 var RegistroRouter = require('./routes/registro');
 var LoginRouter = require('./routes/login');
 var updateRouter = require('./routes/update');
@@ -26,12 +29,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/productos', routerControllers);
 app.use('/registro', RegistroRouter);
 app.use('/login', LoginRouter);
 app.use('/actualizar', updateRouter);
 app.use('/eliminar', removalRouter);
 app.use('/crear', creationRouter);
+
+// Directo controllers 
+app.use('/productos', ProductController);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/products.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var productsControllers = require("./controllers/productsControllers")
-/* GET users listing. */
-router.get('/', productsControllers.getAll);
-
-module.exports = router;
